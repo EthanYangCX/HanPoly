85887696d41a083bba3b4eb721295ac5d42f5f06
yangcxPusto <619980817@qq.com>
2022-02-07 13:10:40 +0000

    add '/testee?han=文' api

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # HanPoly
-Flask, React, 
+
+An experimental project for the lookup of multilingual 
+pronunciations and Middle Chinese rime (phonology features, 韵)
+of Chinese characters. 
+
+Powered by Flask, React, SQLite (later MySQL).
+
+# Current APIs
+
+Homepage：
+
+http://127.0.0.1:5000/
+
+Simple query on [MCPDict](https://github.com/MaigoAkisame/MCPDict)
+(with default query to the chinese character `文`):
+
+http://127.0.0.1:5000/testee?han=六
+
+# Cautions
+
+For Flask app, the paths to files should be written starting from
+the root dir, e.g. `'backend/data/mcpdict.db'`
--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
+
+# DATABASE = './mcpdict.db'
 
 
 @app.route('/')
 def hello_world():
     return '問天地好在。'
+
+
+# def get_db():
+#     db = getattr(g, '_database', None)
+#     if db is None:
+#         db = g._database = sqlite3.connect(DATABASE)
+#     return db
+
+
+# use xxx/testee?han=文
+@app.route('/testee')
+def testee():
+    # cursor = get_db().cursor()
+    han = request.args.get('han')
+    if han is None:
+        han = '文'
+    unicode_str = han.encode('unicode_escape')
+    unicode_str = str(unicode_str)  # like (incl. "b'\\" etc.): b'\\u4e00'
+    unicode_str = unicode_str[5:-1].upper()
+    conn = sqlite3.connect('backend/data/mcpdict.db')
+    cursor = conn.cursor()
+    # cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    cursor.execute('SELECT * FROM mcpdict WHERE unicode = ?', (unicode_str,))
+    # for row in cursor:
+    #     print(row[1])
+    # print(cursor.fetchall())
+    # cursor.execute('SELECT * FROM mcpdict WHERE unicode = ?', '4E00')
+    # values = cursor.fetchall()
+    # print(values)
+    return render_template('testee.html') + str(cursor.fetchall()[0])
 
 
 if __name__ == '__main__':
